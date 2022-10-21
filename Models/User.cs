533e75f8112e55ed013530453235d3a91ff44a67
GH94GitHub <46788915+GH94GitHub@users.noreply.github.com>
2022-10-21 15:39:31 -0500

    Changed user ICollection members to HashSet implementation with helper methods

--- Models/User.cs
+++ Models/User.cs
@@ -34,19 +34,66 @@ namespace FreshMarket.Models
         [Column("payment")]
         public Payment? Payment { get; set; }
 
-
-
-
         public User()
         {
             LastName = "";
-            Allergies = new List<Allergy>();
-            DishPreferences = new List<Dish>();
+            Allergies = new HashSet<Allergy>();
+            DishPreferences = new HashSet<Dish>();
         }
 
         public override string ToString()
         {
             return $"{nameof(Id)}: {Id}, {nameof(Email)}: {Email}, {nameof(Password)}: {Password}, {nameof(FirstName)}: {FirstName}, {nameof(LastName)}: {LastName}, {nameof(Subscription)}: {Subscription}, {nameof(DishPreferences)}: {DishPreferences}, {nameof(Address)}: {Address}, {nameof(PhoneNumber)}: {PhoneNumber}, {nameof(Payment)}: {Payment}, {nameof(Allergies)}: {Allergies}";
+        }
+
+        public bool AddDishPreference(Dish dish)
+        {
+            var beforeCount = this.DishPreferences.Count;
+            this.DishPreferences.Add(dish);
+
+            return beforeCount != this.DishPreferences.Count;
+        }
+        public bool RemoveDishPreference(int dishId)
+        {
+            var beforeCount = this.DishPreferences.Count;
+            for(var i = 0; i < this.DishPreferences.Count; i++)
+            {
+                if (this.DishPreferences.ElementAt(i).Id != dishId) continue;
+                this.DishPreferences.Remove(this.DishPreferences.ElementAt(i));
+                break;
+            }
+
+            return beforeCount != this.DishPreferences.Count;
+        }
+
+        protected bool Equals(User other)
+        {
+            return Id == other.Id && FirstName == other.FirstName && LastName == other.LastName && Password == other.Password && Email == other.Email && DishPreferences.Equals(other.DishPreferences) && Allergies.Equals(other.Allergies) && Subscription.Equals(other.Subscription) && Address == other.Address && PhoneNumber == other.PhoneNumber && Equals(Payment, other.Payment);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((User)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = new HashCode();
+            hashCode.Add(Id);
+            hashCode.Add(FirstName);
+            hashCode.Add(LastName);
+            hashCode.Add(Password);
+            hashCode.Add(Email);
+            hashCode.Add(DishPreferences);
+            hashCode.Add(Allergies);
+            hashCode.Add(Subscription);
+            hashCode.Add(Address);
+            hashCode.Add(PhoneNumber);
+            hashCode.Add(Payment);
+            return hashCode.ToHashCode();
         }
     }
 }
